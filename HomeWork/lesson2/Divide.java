98cf579f50fbd74812589ffa33a64edbde704825
razyke <waqk@mail.ru>
2017-08-22 22:16:48 +0300

    Complete.

--- HomeWork/lesson2/Divide.java
+++ HomeWork/lesson2/Divide.java
@@ -1,15 +1,18 @@
 package lesson2;
 
 public class Divide {
-    //delim
     public static void main(String[] args) {
-        if (args.length!=2){
-            System.out.println("Please use 2 variables");
-        }else {
-            int a = Integer.parseInt(args[0]);
-            int b = Integer.parseInt(args[1]);
-            System.out.println("Di");
+        if (args.length!=2)
+            System.out.println("Please write 2 variables");
+        else {
+            try {
+                double a = Integer.parseInt(args[0]);
+                double b = Integer.parseInt(args[1]);
+                double res = a/b;
+                System.out.println("Divide: "+(int)a+" : "+(int)b+" = "+ (res%1==0? ((int)res) : res+""));
+            }catch (Exception e){
+                System.out.println("Please write 2 integer values");
+            }
         }
-
     }
 }
--- HomeWork/lesson2/Subtraction.java
+++ HomeWork/lesson2/Subtraction.java
@@ -1,5 +1,17 @@
 package lesson2;
 
 public class Subtraction {
-    //vuchitaem
+    public static void main(String[] args) {
+        if (args.length!=2)
+            System.out.println("Please write 2 variables");
+        else {
+            try {
+                int a = Integer.parseInt(args[0]);
+                int b = Integer.parseInt(args[1]);
+                System.out.println("Subtraction: "+a+" - "+b+" = "+ (a-b));
+            }catch (Exception e){
+                System.out.println("Please write 2 integer values");
+            }
+        }
+    }
 }
