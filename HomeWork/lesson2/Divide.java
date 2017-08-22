0b3294ba423979532302c840a72fb4414c6e0018
razyke <waqk@mail.ru>
2017-08-22 18:03:50 +0300

    Start

---
+++ HomeWork/lesson2/Divide.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+public class Divide {
+    //delim
+    public static void main(String[] args) {
+        if (args.length!=2){
+            System.out.println("Please use 2 variables");
+        }else {
+            int a = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[1]);
+            System.out.println("Di");
+        }
+
+    }
+}
